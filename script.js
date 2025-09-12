733af189ce529c427388bb0de99cb50abae26c5f
jumping-riceball0202 <rui62607@gmail.com>
2025-09-13 08:09:58 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const results = ["粉もん", "中華", "イタリアン", "スペイン料理", "ロシア料理", "韓国料理", "エスニック", "未知の味", "トルコ料理", "創作和食", "とんかつ", "すし", "焼肉", "沖縄料理"];
+let lastResult = null;
+
+document.getElementById("omikujiBtn").addEventListener("click", () => {
+  let result;
+  // 直前と同じ結果が出ないようにする
+  do {
+    result = results[Math.floor(Math.random() * results.length)];
+  } while (result === lastResult);
+
+  lastResult = result;
+
+  // 結果を表示
+  document.getElementById("result").innerHTML =
+  `今日の気分は…「<span class="result-word">${result}</span>」です！`;
+});
